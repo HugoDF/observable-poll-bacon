13be907c23665df2ceab008a78658f4e1e7a3307
Glitch (observable-poll-bacon) <none>
2018-06-27 22:46:05 +0000

    🎱🌽 Add client side example

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+
 let count = 0;
 app.get('/count', (req, res) => {
   count++;
   return res.json({ count });
 });
+app.get('/reset', (req, res) => {
+  count = 0;
+  return res.send('Success');
+});
+
+
+app.use(express.static('public'));
 
 app.listen(3000);
